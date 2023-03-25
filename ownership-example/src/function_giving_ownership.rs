3e53347e8c531662fe3213d853d8f40fc6600a49
aleksandar-polic <alpolic8@gmail.com>
2023-03-26 00:40:48 +0100

    Initial examples. Ownership and borrowing.

---
+++ ownership-example/src/function_giving_ownership.rs
@@ -0,0 +1,5 @@
+pub(crate) fn giving_ownership() -> String {
+    let some_str = String::from("hw");
+
+    some_str
+}
---
+++ ownership-example/src/references.rs
@@ -0,0 +1,6 @@
+pub(crate) fn references_main() {
+    //TO DO: Create a function that uses references and the one that doesn't but uses plain values
+    // Connected to docs book 4.2
+
+    println!("Hello from References")
+}
