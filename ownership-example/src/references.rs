9ab0b942821e73f7b97c06e9385cfe3723ab290c
aleksandar-polic <alpolic8@gmail.com>
2023-03-26 12:45:18 +0200

    Add TODO code for references

--- ownership-example/src/references.rs
+++ ownership-example/src/references.rs
@@ -1,6 +1,54 @@
 pub(crate) fn references_main() {
     //TO DO: Create a function that uses references and the one that doesn't but uses plain values
     // Connected to docs book 4.2
+    write_string_and_length_referencing(String::from("Hello references"));
 
-    println!("Hello from References")
+
+    let mut s1 = String::from("Hello references");
+    change_string(&mut s1);
+
+    println!("{s1}");
+}
+
+fn write_string_and_length_owning(s: String) {
+    let (s1, len) = calculate_length(s);
+
+    println!("{s1}: length is {len}");
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let len = s.len();
+
+    (s, len)
+}
+
+// DOES NOT COMPILE
+// fn write_string_and_length_owning(s: String) {
+//     let len = calculate_length(s); // s is moved to function, can't be accessed anymore
+
+//     println!("{s}: length is {len}");
+// }
+
+// fn calculate_length(s: String) -> usize {
+//     let len = s.len();
+
+//     len
+// }
+
+// Working with references
+fn write_string_and_length_referencing(s: String) {
+    let len = calculate_length_referencing(&s);
+
+    println!("{s}: length is {len}");
+}
+
+fn calculate_length_referencing(s: &String) -> usize {
+    let len = s.len();
+
+    len
+}
+
+// Changing the parameter by referencing (mutable references)
+fn change_string(s: &mut String) {
+    s.insert(5, '_');
 }
